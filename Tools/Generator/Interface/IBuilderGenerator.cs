74bf091fc73aa23dd95b09d7375f022ca5d65275
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2022-10-11 20:38:36 -0500

    Builder Intro

---
+++ Tools/Generator/Interface/IBuilderGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tools.Generator.Interface
+{
+    public interface IBuilderGenerator
+    {
+        public void Reset();
+        public void SetContext(List<string> content);
+    }
+}
