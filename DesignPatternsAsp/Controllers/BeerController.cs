09335579b3d2b6bbc96b2414bebb18188b28cdcc
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2022-10-11 19:17:57 -0500

    Add Unit Of Work

--- DesignPatternsAsp/Controllers/BeerController.cs
+++ DesignPatternsAsp/Controllers/BeerController.cs
@@ -1,6 +1,9 @@
-﻿using DesignPatterns.Repository.Interface;
+﻿using DesignPatterns.Models.Data;
+using DesignPatterns.Repository.Interface;
 using DesignPatternsAsp.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,6 +30,49 @@ namespace DesignPatternsAsp.Controllers
                                                };
 
             return View("Index", beers);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var brands = _unitOfWork.Brands.Get();
+            ViewBag.Brands = new SelectList(brands, "BrandId", "BrandName");
+            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(FormBeerViewModel beerVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                var brands = _unitOfWork.Brands.Get();
+                ViewBag.Brands = new SelectList(brands, "BrandId", "BrandName");
+
+                return View("Add", beerVM);
+            }
+
+            var beer = new Beer();
+            beer.BeerName = beerVM.Name;
+            beer.BeerStyle = beerVM.Style;
+
+            if (beerVM.BrandId == null)
+            {
+                var brand = new Brand();
+                brand.BrandName = beerVM.OtherBrand;
+                brand.BrandId = Guid.NewGuid();
+                beer.BrandId = brand.BrandId;
+                _unitOfWork.Brands.Add(brand);
+            }
+            else
+            {
+                beer.BrandId = (Guid)beerVM.BrandId;
+            }
+
+            _unitOfWork.Beers.Add(beer);
+            _unitOfWork.Save();
+
+            return RedirectToAction("Index");
         }
     }
 }
--- DesignPatternsAsp/Models/ViewModels/FormBeerViewModel.cs
+++ DesignPatternsAsp/Models/ViewModels/FormBeerViewModel.cs
@@ -1,6 +1,19 @@
-﻿namespace DesignPatternsAsp.Models.ViewModels
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DesignPatternsAsp.Models.ViewModels
 {
     public class FormBeerViewModel
     {
+        [Required]
+        [Display(Name = "Nombre")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Estilo")]
+        public string Style { get; set; }
+        [Display(Name = "Marca")]
+        public Guid? BrandId { get; set; }
+        [Display(Name = "Otra Marca")]
+        public string OtherBrand { get; set; }
     }
 }
